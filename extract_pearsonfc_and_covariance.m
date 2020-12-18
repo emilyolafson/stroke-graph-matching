a07c7509b9006d406fd2597edf67efd3784c3252
Emily Olafson <olafsonemily@gmail.com>
2020-12-18 14:11:27 -0800

    add gamma fig to outputs

--- extract_pearsonfc_and_covariance.m
+++ extract_pearsonfc_and_covariance.m
@@ -1,21 +1,24 @@
 % convert timeseries to linear correlation & covariance
+nsess=[5;5;5;5;5;4;5;5;5;5;5;3;5;5;5;5;5;5;5;2;5;5;5]
+nsess(24:47)=5;
+
 for i=1:47
     for j=1:nsess(i)
-        ts=load(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/timeseries/session', num2str(j),'/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR.mat'));
+        ts=load(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/data/timeseries/session', num2str(j),'/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR.mat'));
         ts=ts.avg;
         ts=cell2mat(ts);
         C=corrcoef(ts);
-        save(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/pearson_fc/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR_fc.mat'), 'C');
+        save(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/data/pearson_fc/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR_fc.mat'), 'C');
     end
 end
 
 for i=1:47
     for j=1:nsess(i)
-        ts=load(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/timeseries/session', num2str(j),'/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR.mat'));
+        ts=load(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/data/timeseries/session', num2str(j),'/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR.mat'));
         ts=ts.avg;
         ts=cell2mat(ts);
         C=cov(ts);
-        save(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/covariance/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR_fc_cov.mat'), 'C');
+        save(strcat('/Users/emilyolafson/GIT/stroke-graph-matching/data/covariance/SUB', num2str(i),'_S', num2str(j), '_shen268_GSR_fc_cov.mat'), 'C');
     end
 end
 
