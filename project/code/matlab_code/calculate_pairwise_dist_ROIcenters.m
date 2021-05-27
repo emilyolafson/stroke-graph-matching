a05c15f0716fbe6c7513968baed293a1e091c413
Emily Olafson <emily.olafson@mail.mcgill.ca>
2021-05-27 16:24:34 -0400

    calculate distance between ROI centers

--- project/code/matlab_code/calculate_pairwise_dist_ROIcenters.m
+++ project/code/matlab_code/calculate_pairwise_dist_ROIcenters.m
@@ -1,6 +1,6 @@
 % calculate pairwise euclidean distances betweeen ROI centers in Shen268
 % atlas.
-
+load('/Users/emilyolafson/GIT/stroke-graph-matching/data/atlasblobs.mat', 'atlasblobs_list')
 roicenters = atlasblobs_list(8).roicenters;
 
 eucl_dist=zeros(268,268);
@@ -11,9 +11,4 @@ for i=1:268
     end
 end
 
-eucl_dist_norm=eucl_dist./max(max(eucl_dist))
-
-plot_yeo(eucl_dist, 'Euclidean distances between mean centers of Shen268 nodes', 'summer', [0 100], 'r')
-saveas(gcf,'results/pairwise_euclidean_dist_shen268nodes.png')
-save('/Users/emilyolafson/GIT/stroke-graph-matching/results/pairwise_eucl_dist_shen268.mat', 'eucl_dist')
-save('/Users/emilyolafson/GIT/stroke-graph-matching/results/pairwise_eucl_dist_shen268_normalized.mat', 'eucl_dist_norm')
+save('/Users/emilyolafson/GIT/stroke-graph-matching/data/pairwise_eucl_dist_shen268.mat', 'eucl_dist')
